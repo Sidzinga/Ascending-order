08a5897d6fb93b59698187e3a9594ba8f575a834
Sidzinga Maseko <125248940+Sidzinga@users.noreply.github.com>
2023-09-18 13:09:51 +0200

    Add files via upload

---
+++ Alpha-Order.js
@@ -0,0 +1,8 @@
+function alphaOrder(arr){
+return arr.sort() //The sort method returns the array in alphabetical order
+}
+
+let array = ["Gift", "Candice","Sidzinga","Mark","Amos","Wongani","Kgothatso","David","Zulu","Ben","Qalinge"]
+// array is stored in a variable to be called on any time
+
+console.log(alphaOrder(array))
